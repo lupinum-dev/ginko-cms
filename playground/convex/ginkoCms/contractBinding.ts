f26edba5597591bb0694a3c45243231517c2d663
Mat4m0 <matthias@lupinum.com>
2026-08-23 06:10:48 +0200

    fix(playground): bind the current CMS contract

--- playground/convex/ginkoCms/contractBinding.ts
+++ playground/convex/ginkoCms/contractBinding.ts
@@ -1,6 +1,6 @@
 import { query } from '../_generated/server.js'
 
-const EXPECTED_CONTENT_HASH = '3428fce0867f71bb4b7b2df8f39fbbdc6044380a6d3da3d8401887d239934efa'
+const EXPECTED_CONTENT_HASH = 'ff6cdc31b73b3b517f99652518ea1b5d7cb11a0f4ab761b0ff2b996e0ec69cb8'
 const PRESENTATION_HASH = 'ce5dc433d79767ec1e8dbbeb4dad13d69c5f41ac70f8607a5b2b6ac6ed2a04c6'
 const SHA256 = /^[a-f0-9]{64}$/u
 
